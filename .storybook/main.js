cde755f0923bd92df0ac0707c3592abcdfcb4e78
Mustafa Alshammaa <adam.benals@gmail.com>
2021-03-23 14:46:39 +0100

    Added the essential webpack configuration to make Storybook work with Vue and Vuetify

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,5 +8,56 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+
+  webpackFinal: async (config, { configType }) => {
+    // Use Sass loader for vuetify components
+    config.module.rules.push({
+      test: /\.sass$/,
+      use: [
+        'style-loader',
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          // Requires sass-loader@^9.0.0
+          options: {
+            // This is the path to your variables
+            additionalData: `@import '@/styles/variables.scss'`
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          // Requires sass-loader@^9.0.0
+          options: {
+            // This is the path to your variables
+            additionalData: `@import '@/styles/variables.scss';`
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    config.module.rules.push({
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '../src'),
+          vue: 'vue/dist/vue.js',
+          'vue$': 'vue/dist/vue.esm.js',
+        },
+      },
+    });
+
+    // Return the altered config
+    return config;
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,21 @@
+import vuetifyConfig from '../src/plugins/vuetify';
+import { addDecorator } from '@storybook/vue';
+import { VApp, VMain, VContainer } from "vuetify/lib";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+addDecorator(() => ({
+  vuetify: vuetifyConfig,
+  components: { VApp, VMain, VContainer },
+  template: `
+    <v-app>
+      <v-main>
+        <v-container fluid>
+          <story/>
+        </v-container>
+      </v-main>
+    </v-app>
+    `
+}));
